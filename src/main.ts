5e8dfe8ad437df4e7a99f49650e60f3d644885d0
Caroline Robillard <line.robillard@gmail.com>
2024-08-05 23:26:05 +0200

    delete wording

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     // logger: ['log', 'error', 'warn', 'debug', 'verbose'] ,
   });
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix('api');
 
   app.enableCors({
     origin: [
