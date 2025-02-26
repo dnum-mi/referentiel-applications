b9ff8db3464d3b593e0752081a38d7dd1b5181b1
Thomas Bernard <thomashbernard@gmail.com>
2025-02-26 15:37:10 +0100

    fix: :bug: ensure app is closed only if it exists in afterEach cleanup

--- backend/tests/anomaly-notifications.e2e-spec.ts
+++ backend/tests/anomaly-notifications.e2e-spec.ts
@@ -3,6 +3,9 @@ import { setupTestSuite } from './setup';
 import { v4 as uuidv4 } from 'uuid';
 import { PrismaService } from 'src/prisma/prisma.service';
 
+const TOKEN =
+  'Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIwYXBzYjYxQzVFa2x6d2JjRUpXYXZPeXllMk5UQ29FRHpRb2lFdWFlTjJnIn0.eyJzdWIiOiI5OWZhNDE5ZS1mNGZiLTRlZDctOGIxMS1jNzIxMGIzMDkiLCAiZW1haWwiOiJ0aG9tYXMuYmVybmFyZC1lY29ub2NvbUBpbnRlcmlldXIuZ291di5mciJ9.';
+
 describe('Anomaly Notifications', () => {
   const getApp = setupTestSuite();
   const keycloakId = uuidv4();
@@ -21,10 +24,7 @@ describe('Anomaly Notifications', () => {
     const app = getApp();
     return request(app.getHttpServer())
       .get('/anomaly-notifications')
-      .set(
-        'Authorization',
-        'Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIwYXBzYjYxQzVFa2x6d2JjRUpXYXZPeXllMk5UQ29FRHpRb2lFdWFlTjJnIn0.eyJzdWIiOiI5OWZhNDE5ZS1mNGZiLTRlZDctOGIxMS1jNzIxMGIzMDkiLCAiZW1haWwiOiJ0aG9tYXMuYmVybmFyZC1lY29ub2NvbUBpbnRlcmlldXIuZ291di5mciJ9.',
-      )
+      .set('Authorization', TOKEN)
       .expect(200);
   });
 });
