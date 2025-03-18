f4302956ebe608aada599ef95988131c9e6f98ad
Thomas Bernard <thomashbernard@gmail.com>
2025-03-18 16:21:29 +0100

    feat: :sparkles: implement JWT authentication using jose library and refactor auth middleware

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -4,20 +4,31 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
-import { AuthUtils } from '../utils/helpers';
+import { createRemoteJWKSet, jwtVerify } from 'jose';
 import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
-  constructor(private userService: UserService) {}
+  private static JWKS: ReturnType<typeof createRemoteJWKSet> | null = null;
+
+  constructor(private userService: UserService) {
+    if (!AuthMiddleware.JWKS) {
+      AuthMiddleware.JWKS = createRemoteJWKSet(
+        new URL(process.env.KEYCLOAK_JWKS_URL),
+      );
+    }
+  }
 
   async use(req: Request, res: Response, next: NextFunction) {
     try {
-      const decodedToken = AuthUtils.getDecodedToken(req);
-      req.user = await AuthUtils.findOrCreateUser(
-        decodedToken,
-        this.userService,
+      const token = req.headers['authorization'].split(' ')[1];
+      const { payload } = await jwtVerify(token, AuthMiddleware.JWKS);
+
+      req.user = await this.userService.findOrCreateByEmail(
+        payload.email as string,
+        payload.sub as string,
       );
+
       next();
     } catch {
       throw new UnauthorizedException("L'authentification a échoué");
