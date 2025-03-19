8b954c5c09c3b72013c935863e6be0fb66128681
Thomas Bernard <thomashbernard@gmail.com>
2025-03-19 12:49:29 +0100

    refactor: :recycle: simplify e2e tests by introducing user and application fakers

--- backend/tests/anomaly-notifications.e2e-spec.ts
+++ backend/tests/anomaly-notifications.e2e-spec.ts
@@ -1,22 +1,9 @@
 import request from 'supertest';
 import { setupTestSuite } from './setup';
-import { v4 as uuidv4 } from 'uuid';
-import { PrismaService } from 'src/prisma/prisma.service';
 import { getToken } from './getToken';
 
 describe('Anomaly Notifications', () => {
   const getApp = setupTestSuite();
-  const keycloakId = uuidv4();
-
-  beforeAll(async () => {
-    const prismaService = new PrismaService();
-    await prismaService.user.create({
-      data: {
-        email: `${keycloakId}@test.fr`,
-        keycloakId: keycloakId,
-      },
-    });
-  });
 
   it(`/GET anomaly-notifications`, async () => {
     const app = getApp();
