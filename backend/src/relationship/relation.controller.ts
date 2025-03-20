0cad2573b09b697a6f8be8ee15293f7ea60c899c
Caroline Robillard <line.robillard@gmail.com>
2025-03-20 15:44:12 +0100

    feat: :sparkles: update or remove relations

--- backend/src/relationship/relation.controller.ts
+++ backend/src/relationship/relation.controller.ts
@@ -1,8 +1,16 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { RelationService } from './relation.service';
-import { CreateRelationDto } from './application/dto/relation-application.dto';
+import { RelationApplicationDto } from './application/dto/relation-application.dto';
 import { Relation } from './domain/relation.entity';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('relation')
 @Controller('relations')
@@ -10,7 +18,46 @@ export class RelationController {
   constructor(private readonly relationService: RelationService) {}
 
   @Post()
-  async create(@Body() dto: CreateRelationDto): Promise<Relation> {
+  @ApiOperation({ summary: 'Créer une nouvelle relation' })
+  async create(@Body() dto: RelationApplicationDto): Promise<Relation> {
     return this.relationService.create(dto);
+  }
+
+  @Get()
+  @ApiOperation({ summary: 'Récupérer toutes les relations' })
+  async findAll(): Promise<Relation[]> {
+    return this.relationService.findAll();
+  }
+
+  @Get(':id')
+  @ApiOperation({
+    summary: 'Récupérer une relation par son identifiant unique',
+  })
+  @ApiParam({ name: 'id', description: 'Identifiant unique de la relation' })
+  async findOne(@Param('id') id: string): Promise<Relation> {
+    return this.relationService.findOne(id);
+  }
+
+  @Patch(':id')
+  @ApiOperation({ summary: 'Mettre à jour une relation' })
+  @ApiParam({
+    name: 'id',
+    description: 'Identifiant unique de la relation à mettre à jour',
+  })
+  async update(
+    @Param('id') id: string,
+    @Body() dto: RelationApplicationDto,
+  ): Promise<Relation> {
+    return this.relationService.update(id, dto);
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: 'Supprimer une relation' })
+  @ApiParam({
+    name: 'id',
+    description: 'Identifiant unique de la relation à supprimer',
+  })
+  async delete(@Param('id') id: string): Promise<void> {
+    return this.relationService.delete(id);
   }
 }
