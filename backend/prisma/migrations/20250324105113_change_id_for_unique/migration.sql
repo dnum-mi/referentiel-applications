c6d6a9ae466f97ed20cf054e4cc46bf40f048764
Caroline Robillard <line.robillard@gmail.com>
2025-03-25 15:42:13 +0100

    fix: :bug: Add optional `id` to relations and populate before setting as primary key

--- backend/prisma/migrations/20250324105113_change_id_for_unique/migration.sql
+++ backend/prisma/migrations/20250324105113_change_id_for_unique/migration.sql
@@ -1,15 +1,16 @@
-/*
-  Warnings:
-
-  - The primary key for the `relations` table will be changed. If it partially fails, the table could be left without primary key constraint.
-  - A unique constraint covering the columns `[applicationSource,applicationTarget,type]` on the table `relations` will be added. If there are existing duplicate values, this will fail.
-  - The required column `id` was added to the `relations` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
-
-*/
 -- AlterTable
-ALTER TABLE "relations" DROP CONSTRAINT "relations_pkey",
-ADD COLUMN     "id" TEXT NOT NULL,
+ALTER TABLE "relations"
+ADD COLUMN "id" TEXT;
+
+
+UPDATE "relations"
+SET "id" = gen_random_uuid()
+WHERE "id" IS NULL;
+
+ALTER TABLE "relations"
+DROP CONSTRAINT "relations_pkey",
+ALTER COLUMN "id" SET NOT NULL,
 ADD CONSTRAINT "relations_pkey" PRIMARY KEY ("id");
 
--- CreateIndex
-CREATE UNIQUE INDEX "relations_applicationSource_applicationTarget_type_key" ON "relations"("applicationSource", "applicationTarget", "type");
+CREATE UNIQUE INDEX "relations_applicationSource_applicationTarget_type_key"
+ON "relations"("applicationSource", "applicationTarget", "type");
