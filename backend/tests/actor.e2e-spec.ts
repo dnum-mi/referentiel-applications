848de7c9c130fee7404e8f4765772deb648859ea
Thomas Bernard <thomashbernard@gmail.com>
2025-03-26 13:02:52 +0100

    refactor: :recycle: simplify actor e2e tests by using fakers and removing redundant code

--- backend/tests/actor.e2e-spec.ts
+++ backend/tests/actor.e2e-spec.ts
@@ -1,47 +1,30 @@
 import request from 'supertest';
 import { setupTestSuite } from './setup';
-import { v4 as uuidv4 } from 'uuid';
-import { PrismaService } from 'src/prisma/prisma.service';
-
-const TOKEN =
-  'Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIwYXBzYjYxQzVFa2x6d2JjRUpXYXZPeXllMk5UQ29FRHpRb2lFdWFlTjJnIn0.eyJzdWIiOiI5OWZhNDE5ZS1mNGZiLTRlZDctOGIxMS1jNzIxMGIzMDkiLCAiZW1haWwiOiJ0aG9tYXMuYmVybmFyZC1lY29ub2NvbUBpbnRlcmlldXIuZ291di5mciJ9.';
+import { getToken } from './getToken';
+import { UserFaker } from './fakers/user.faker';
+import { ApplicationFaker } from './fakers/application.faker';
 
 describe('Actor', () => {
-  const getApp = setupTestSuite();
-  const keycloakId = uuidv4();
-  const applicationId = uuidv4();
+  const app = setupTestSuite();
+  let application: { id: string };
+  let user: { keycloakId: string };
 
   beforeAll(async () => {
-    const prismaService = new PrismaService();
-    await prismaService.user.create({
-      data: {
-        email: `${keycloakId}@test.fr`,
-        keycloakId: keycloakId,
-      },
-    });
-    await prismaService.application.create({
-      data: {
-        id: applicationId,
-        label: 'Test Application',
-        description: 'Test Application Description',
-        owner: {
-          connect: {
-            keycloakId: keycloakId,
-          },
-        },
-        metadata: {
-          create: {
-            createdById: keycloakId,
-            updatedById: keycloakId,
-          },
-        },
-      },
-    });
+    user = await UserFaker.create();
+    application = await ApplicationFaker.create(user);
   });
 
-  it(`/POST actor`, () => {
-    const app = getApp();
-    return request(app.getHttpServer())
+  it(`/GET actor`, async () => {
+    const TOKEN = await getToken();
+    await request(app().getHttpServer())
+      .get('/actor')
+      .set('Authorization', `Bearer ${TOKEN}`)
+      .expect(200);
+  });
+
+  it(`/POST actor`, async () => {
+    const TOKEN = await getToken();
+    await request(app().getHttpServer())
       .post('/actor')
       .send({
         role: 'test',
@@ -50,17 +33,9 @@ describe('Actor', () => {
         lastname: 'lastname',
         type: 'Autre',
         organizationId: '',
-        applicationId: applicationId,
+        applicationId: application.id,
       })
-      .set('Authorization', TOKEN)
+      .set('Authorization', `Bearer ${TOKEN}`)
       .expect(201);
-  });
-
-  it(`/GET actor`, () => {
-    const app = getApp();
-    return request(app.getHttpServer())
-      .get('/actor')
-      .set('Authorization', TOKEN)
-      .expect(200);
   });
 });
