e728df0176c6b1d7393bd87b66ace50eefa659f5
Thomas Bernard <thomashbernard@gmail.com>
2025-03-26 12:47:56 +0100

    refactor: :recycle: streamline organization service methods and remove redundant comments

--- backend/src/organization/organization.service.ts
+++ backend/src/organization/organization.service.ts
@@ -10,84 +10,29 @@ import {
 export class OrganizationService {
   constructor(private OrganisationRepository: OrganizationRepository) {}
 
-  /**
-   * Crée une nouvelle Organisation
-   *
-   * @param CreateOrganizationDto Les données nécessaires à la création de l'organisation
-   *
-   * @returns L'organisation nouvellement créée
-   * @throws A DÉTERMINER
-   */
-  public async createOrganization(createOrganization: CreateOrganizationDto) {
+  public async create(createOrganization: CreateOrganizationDto) {
     return await this.OrganisationRepository.create(createOrganization);
   }
 
-  /**
-   * Met à jour une organisation existante
-   *
-   * @param params Contient l'ID de l'organisation et les données à mettre à jour
-   *
-   * @returns L'organisation mise à jour
-   * @throws NotFoundException Si l'organisation à mettre à jour n'est pas trouvée
-   */
-  public async updateOrganization(params: {
-    where: Prisma.OrganizationWhereUniqueInput;
-    data: PatchOrganizationDto;
-  }): Promise<Organization> {
-    const { where, data } = params;
-
-    try {
-      return await this.OrganisationRepository.update(where, data);
-    } catch {
-      throw new NotFoundException(
-        `Organisation non trouvée pour l'ID ${where.id}`,
-      );
-    }
-  }
-
-  /**
-   * Récupère une application spécifique par son ID
-   *
-   * @param id L'identifiant de l'organisation à récuperer
-   *
-   * @returns L'organisation trouvée
-   * @throws NotFoundException Si l'organisation n'est pas trouvée
-   */
-  public async getOrganizationById(id: string) {
+  public async findOne(id: string) {
     const organization = this.OrganisationRepository.findById(id);
-
     if (!organization) {
       throw new NotFoundException(`Organisation non trouvée pour l'ID ${id}`);
     }
-
     return organization;
   }
 
-  /**
-   * Récupère toutes les organisations
-   *
-   * @returns La liste de toutes les organisations
-   * @throws Error Si une erreur survient pendant la récupération des organisations
-   */
-  public async getOrganizations() {
+  public async findAll() {
     return await this.OrganisationRepository.findAll();
   }
 
-  /**
-   * Supprime une organisation
-   *
-   * @param id L'identifiant de l'organisation à supprimer
-   *
-   * @returns L'organisation supprimée
-   * @throws NotFoundException Si l'organisation à supprimer n'est pas trouvée
-   */
-  public async deleteOrganization(id: string) {
-    const organization = await this.OrganisationRepository.delete(id);
+  public async update(id, data: PatchOrganizationDto): Promise<Organization> {
+    await this.findOne(id);
+    return await this.OrganisationRepository.update(id, data);
+  }
 
-    if (!organization) {
-      throw new NotFoundException(`Organisation non trouvée pour l'ID ${id}`);
-    }
-
-    return organization;
+  public async delete(id: string) {
+    await this.findOne(id);
+    return await this.OrganisationRepository.delete(id);
   }
 }
