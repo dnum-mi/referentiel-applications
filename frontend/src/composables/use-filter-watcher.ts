6c95a927699dafd35155fd0e110834c1654d3c82
Caroline Robillard <line.robillard@gmail.com>
2025-04-12 15:34:21 +0200

    refactor(front): :art: write watcher for filter in composable

---
+++ frontend/src/composables/use-filter-watcher.ts
@@ -0,0 +1,20 @@
+import { watch } from "vue";
+import { useDebouncedFn } from "@/composables/use-debouncefn";
+
+export function useFilterWatcher(filters: Record<string, any>, callback: () => void, keysToWatch?: string[]) {
+  const debounced = useDebouncedFn(callback, 300);
+  const keys = keysToWatch || Object.keys(filters);
+
+  keys.forEach((key) => {
+    const isArray = Array.isArray(filters[key]) || Array.isArray(filters[key]?.value);
+
+    watch(
+      () => filters[key], // ✅ on observe bien la propriété réactive
+      () => {
+        console.log(`🌀 [Watcher] Changement sur "${key}" → appel API`);
+        debounced.run();
+      },
+      { deep: isArray },
+    );
+  });
+}
