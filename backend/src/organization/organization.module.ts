f250aa0253e7484a871e092a1cd18c0bcf50f45c
Thomas Bernard <thomashbernard@gmail.com>
2025-04-18 15:19:49 +0200

    refactor: :recycle: remove organization repository and mapping, simplify service structure

--- backend/src/organization/organization.module.ts
+++ backend/src/organization/organization.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { OrganizationController } from './organization.controller';
 import { OrganizationService } from './organization.service';
-import { OrganizationRepository } from './infrastructure/repository/organization.repository';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [OrganizationController],
-  providers: [OrganizationService, OrganizationRepository, PrismaService],
-  exports: [OrganizationRepository],
+  providers: [OrganizationService, PrismaService],
 })
 export class OrganizationModule {}
