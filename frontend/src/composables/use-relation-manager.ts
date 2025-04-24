0605f36521b3c4440465f240e0857080e63facb1
Thomas Bernard <thomashbernard@gmail.com>
2025-04-24 13:04:58 +0200

    fix: :bug: correct target label for 'in_replacement_of' relation type

--- frontend/src/composables/use-relation-manager.ts
+++ frontend/src/composables/use-relation-manager.ts
@@ -20,7 +20,7 @@ export function useRelationManager(application: Application, emit: (event: strin
 
   const relationTypes: Record<string, { source: string; target: string }> = {
     is_part_of: { source: "Fait partie de", target: "A comme sous‑élément" },
-    in_replacement_of: { source: "Remplace", target: "Remplace" },
+    in_replacement_of: { source: "Remplace", target: "est remplacé par" },
     is_service_user_of: { source: "Utilise le service de", target: "Fournit le service à" },
     is_data_user_of: { source: "Utilise la donnée de", target: "Fournit la donnée à" },
   };
