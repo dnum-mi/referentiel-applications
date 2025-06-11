a9e55fe864061c9da4f2f1dfbdce8bd2bd55b512
melissa <melissataileb938@gmail.com>
2025-06-11 09:08:56 +0200

    style: format code

--- backend/src/product/application.controller.ts
+++ backend/src/product/application.controller.ts
@@ -35,7 +35,7 @@ export class ApplicationController {
     private readonly applicationService: ApplicationService,
     private readonly exportApplicationsUseCase: ExportApplicationsUseCase,
     private readonly applicationExportService: ApplicationExportService,
-  ) { }
+  ) {}
 
   @Post()
   @ApiBody({ type: CreateApplicationDto })
@@ -141,8 +141,8 @@ Le paramètre **id** doit être fourni dans l'URL.
     const buffer =
       Object.keys(searchParams).length > 0
         ? await this.applicationExportService.exportSearchResultsToExcel(
-          searchParams,
-        )
+            searchParams,
+          )
         : await this.exportApplicationsUseCase.execute();
 
     res.setHeader(
