5d2e55ff087b3e0ecfeed286effebd0776f3d180
Thomas Bernard <thomashbernard@gmail.com>
2025-07-07 18:32:15 +0200

    fix: :bug: fix modal behavior when pressing enter

--- frontend/src/components/form/ApplicationInfoForm.vue
+++ frontend/src/components/form/ApplicationInfoForm.vue
@@ -177,7 +177,7 @@ const removePopulation = (index: number) => {
     </div>
 
     <div class="fr-btns-group fr-btns-group--right fr-mt-4w">
-      <DsfrButton secondary label="Annuler" @click="$emit('cancel')" />
+      <DsfrButton type="button" secondary label="Annuler" @click="$emit('cancel')" />
       <DsfrButton type="submit" :disabled="isSubmitting" :label="isSubmitting ? 'Enregistrement...' : 'Enregistrer'">
         <template v-if="isSubmitting">
           <span class="fr-loading fr-loading--sm">
