c44b0f67cf5656095e33cfbb3208ad36645dd98d
Caroline Robillard <line.robillard@gmail.com>
2025-07-09 16:38:44 +0200

    fix(front): :bug: fix tabulation for tabs

--- frontend/src/components/ApplicationOverview.vue
+++ frontend/src/components/ApplicationOverview.vue
@@ -1,21 +1,90 @@
 <script setup lang="ts">
-import type { Application } from "@/models/Application";
 import { ref, watch, onMounted } from "vue";
-import { useRouter, useRoute } from "vue-router";
-import ActorManager from "./actor/ActorTab.vue";
-import Compliances from "./CompliancesTab.vue";
+import { useRoute, useRouter } from "vue-router";
+import type { Application } from "@/models/Application";
+
 import InformationsGenerales from "./InformationsGenerales.vue";
 import Links from "./LinksTab.vue";
-import NotificationsApplication from "./NotificationsApplication.vue";
+import Compliances from "./CompliancesTab.vue";
+import ActorManager from "./actor/ActorTab.vue";
 import Relationships from "./RelationshipsTab.vue";
+import NotificationsApplication from "./NotificationsApplication.vue";
 import Quality from "./QualityTab.vue";
 
 const props = defineProps<{ application: Application }>();
 const emit = defineEmits(["update:application"]);
-const application = ref<Application>(props.application);
+
+const application = ref(props.application);
 const activeTab = ref(0);
-const router = useRouter();
 const route = useRoute();
+const router = useRouter();
+
+const updateApplication = (updatedApp: Application) => {
+  Object.assign(application.value, updatedApp);
+  emit("update:application", updatedApp);
+};
+
+const tabs = [
+  {
+    title: "Informations générales",
+    icon: "ri-checkbox-circle-line",
+    tabId: "tab-infos",
+    panelId: "panel-infos",
+    component: InformationsGenerales,
+  },
+  {
+    title: "Liens",
+    icon: "ri-links-line",
+    tabId: "tab-links",
+    panelId: "panel-links",
+    component: Links,
+  },
+  {
+    title: "Conformités",
+    icon: "ri-shield-check-line",
+    tabId: "tab-compliances",
+    panelId: "panel-compliances",
+    component: Compliances,
+  },
+  {
+    title: "Acteurs",
+    icon: "ri-team-line",
+    tabId: "tab-actors",
+    panelId: "panel-actors",
+    component: ActorManager,
+  },
+  {
+    title: "Relations",
+    icon: "ri-node-tree",
+    tabId: "tab-relations",
+    panelId: "panel-relations",
+    component: Relationships,
+  },
+  {
+    title: "Historiques",
+    icon: "ri-edit-line",
+    tabId: "tab-history",
+    panelId: "panel-history",
+    component: NotificationsApplication,
+  },
+  {
+    title: "Qualité",
+    icon: "ri-bar-chart-line",
+    tabId: "tab-quality",
+    panelId: "panel-quality",
+    component: Quality,
+  },
+];
+
+onMounted(() => {
+  const tabParam = route.query.tab;
+  if (tabParam && !isNaN(Number(tabParam))) {
+    const index = Number(tabParam);
+    if (index >= 0 && index < tabs.length) {
+      activeTab.value = index;
+    }
+  }
+});
 
 watch(
   () => props.application,
@@ -24,74 +93,18 @@ watch(
   },
 );
 
-// Watch for changes to the activeTab and update the URL
 watch(
   () => activeTab.value,
-  (newTab) => {
-    router.replace({
-      query: { ...route.query, tab: newTab.toString() },
-    });
+  (newVal) => {
+    router.replace({ query: { ...route.query, tab: newVal.toString() } });
   },
 );
-
-onMounted(() => {
-  // Read the tab from URL on component mount
-  const tabParam = route.query.tab;
-  if (tabParam && !isNaN(Number(tabParam))) {
-    const tabIndex = Number(tabParam);
-    if (tabIndex >= 0 && tabIndex < tabs.length) {
-      activeTab.value = tabIndex;
-    }
-  }
-});
-
-const updateApplication = (updatedApp: Application) => {
-  Object.assign(application.value, updatedApp);
-  emit("update:application", updatedApp);
-  console.log(updatedApp);
-};
-
-const tabs = [
-  { title: "Informations générales", icon: "ri-checkbox-circle-line", component: InformationsGenerales },
-  {
-    title: "Liens",
-    icon: "ri-links-line",
-    component: Links,
-  },
-  {
-    title: "Conformités",
-    icon: "ri-shield-check-line",
-    component: Compliances,
-  },
-  {
-    title: "Acteurs",
-    icon: "ri-team-line",
-    component: ActorManager,
-  },
-  { title: "Relations", icon: "ri-node-tree", component: Relationships },
-
-  { title: "Historiques", icon: "ri-edit-line", component: NotificationsApplication },
-
-  { title: "Qualité", icon: "ri-bar-chart-line", component: Quality },
-];
 </script>
-<template>
-  <DsfrTabs v-model="activeTab" tab-list-name="Informations sur l'application">
-    <template #tab-items>
-      <DsfrTabItem
-        v-for="(tab, index) in tabs"
-        :key="index"
-        :tab-id="`tab-${index}`"
-        :panel-id="`tab-content-${index}`"
-        :icon="tab.icon"
-        @click="activeTab = index"
-      >
-        {{ tab.title }}
-      </DsfrTabItem>
-    </template>
 
-    <template v-for="(tab, index) in tabs" :key="index">
-      <DsfrTabContent v-show="activeTab === index" :panel-id="`tab-content-${index}`" :tab-id="`tab-${index}`">
+<template>
+  <DsfrTabs v-model="activeTab" tab-list-name="Informations sur l'application" :tab-titles="tabs">
+    <template v-for="(tab, index) in tabs" :key="tab.panelId">
+      <DsfrTabContent :tab-id="tab.tabId" :panel-id="tab.panelId" v-show="activeTab === index">
         <component :is="tab.component" :application="application" @update:application="updateApplication" />
       </DsfrTabContent>
     </template>
