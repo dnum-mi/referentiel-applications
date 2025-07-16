7fee84d16b560949939bc6c8b8e69a5fcd125138
Nonot <33383276+ArnaudTA@users.noreply.github.com>
2025-07-16 13:58:39 +0200

    refactor: :recycle: rename store  userStore (#851)

--- frontend/src/stores/userStore.ts
+++ frontend/src/stores/userStore.ts
@@ -2,7 +2,7 @@ import Users from "@/api/user";
 import type { User } from "@/models/user";
 import { authentication } from "@/services/authentication";
 
-export const useUserStore = defineStore("reportIssueStore", () => {
+export const useUserStore = defineStore("userStore", () => {
   const user = ref<User>();
   const authenticated = ref(authentication.authenticated);
   const userPermissions = computed<string[]>(() => user.value?.permissions.split(",") ?? []);
