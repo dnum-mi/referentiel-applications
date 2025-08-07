a9ba05dccb78e933f5686a28f15965a52198b854
ArnaudTA <33383276+ArnaudTA@users.noreply.github.com>
2025-08-07 14:47:44 +0200

    fix: :bug: remove admin guard from stats controller

--- backend/src/stats/interfaces/stats.controller.ts
+++ backend/src/stats/interfaces/stats.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, UseGuards } from "@nestjs/common";
+import { Controller, Get, Query } from "@nestjs/common";
 import { StatsService } from "../application/stats.service";
 import { GroupBy } from "./types/stats-entry.type";
 import {
@@ -7,13 +7,9 @@ import {
   ApiQuery,
   ApiTags,
 } from "@nestjs/swagger";
-import { AdminLevel } from "src/user/entities/user.entity";
-import { AdminGuard } from "src/common/guards/admin.guard";
-import { RequiredAdminLevel } from "src/common/decorators/admin.decorator";
+import { GetIqAvgGroupedDto } from "../application/dto/get-iq-avg-grouped.dto";
 
 @ApiTags("Stats")
-@UseGuards(AdminGuard)
-@RequiredAdminLevel(AdminLevel.ADMIN)
 @Controller("stats")
 export class StatsController {
   constructor(private readonly statsService: StatsService) {}
@@ -66,6 +62,8 @@ Authorization: Bearer <TOKEN>
         { label: "2025-07", moyenne: 52.5, min: 50, max: 55, count: 2 },
       ],
     },
+    type: GetIqAvgGroupedDto,
+    isArray: true,
   })
   async getIqAvgGrouped(
     @Query("from") from?: string,
