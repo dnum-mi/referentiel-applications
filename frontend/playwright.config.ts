620f322c9e15aa7584b8e89b28fa36503a379c27
Thomas Bernard <thomashbernard@gmail.com>
2025-09-24 14:55:13 +0200

    fix: ✅ fix playwright ci tests

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -25,9 +25,7 @@ export default defineConfig({
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
-
+    baseURL: "http://localhost:5173",
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
@@ -71,9 +69,8 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "pnpm run dev",
+    url: "http://localhost:5173",
+  },
 });
