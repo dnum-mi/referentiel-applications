246f6b916d90653bf6afcd21dd2082466520c798
Thomas <thomashbernard@gmail.com>
2025-10-02 11:58:40 +0200

    fix: :bug: fix application creation when no tags filled (#1122)

---
+++ frontend/src/components/modal/CreateApplicationModal.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import ApplicationInfoForm from "@/components/form/ApplicationInfoForm.vue";
+
+interface Props {
+  opened: boolean
+}
+
+defineProps<Props>();
+const emit = defineEmits<{
+  close: []
+}>();
+</script>
+
+<template>
+  <DsfrModal
+    size="lg"
+    :opened="opened"
+    title="Créer une application"
+    data-testid="create-application-modal"
+    @close="emit('close');"
+  >
+    <ApplicationInfoForm
+      data-testid="create-application-form"
+      @success="emit('close')"
+      @cancel="emit('close')"
+    />
+  </DsfrModal>
+</template>
