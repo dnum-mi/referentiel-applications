f191d7beb82f752864dedca2d2671aaad76d24a5
leo P <Theoasis953@gmail.com>
2025-11-03 10:30:17 +0100

    fix: :wheelchair: replace non-dsfr tables with dsfr vue component and correct invalid html structure

--- frontend/src/views/UserProfile.vue
+++ frontend/src/views/UserProfile.vue
@@ -16,49 +16,42 @@ onMounted(() => {
       </h1>
       <div v-if="userStore.user" class="fr-card" data-testid="user-profile-card">
         <div class="fr-card__body fr-mt-2w">
-          <h2 class="fr-h4" data-testid="user-profile-title">
-            Informations personnelles
-          </h2>
-          <div class="fr-table">
-            <table>
-              <tbody>
-                <tr>
-                  <th scope="row">
-                    ID Keycloak
-                  </th>
-                  <td data-testid="user-profile-keycloak">
-                    {{ userStore.user.keycloakId }}
-                  </td>
-                </tr>
-                <tr>
-                  <th scope="row">
-                    Organisation
-                  </th>
-                  <td data-testid="user-profile-organization">
-                    {{ userStore.user.organization?.label || "Non renseignée" }}
-                  </td>
-                </tr>
-                <tr>
-                  <th scope="row">
-                    Email
-                  </th>
-                  <td data-testid="user-profile-email">
-                    {{ userStore.user.email }}
-                  </td>
-                </tr>
-                <tr>
-                  <th scope="row">
-                    Type
-                  </th>
-                  <td>
-                    <span class="fr-badge fr-mr-1w" :class="AdminLevelWordingBadgeClass[userStore.adminLevel]" data-testid="user-profile-type">
-                      {{ AdminLevelWording[userStore.adminLevel] }}
-                    </span>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+          <DsfrTable title="Informations personnelles" data-testid="user-profile-table">
+            <tr>
+              <th scope="row">
+                ID Keycloak
+              </th>
+              <td data-testid="user-profile-keycloak">
+                {{ userStore.user.keycloakId }}
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">
+                Organisation
+              </th>
+              <td data-testid="user-profile-organization">
+                {{ userStore.user.organization?.label || "Non renseignée" }}
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">
+                Email
+              </th>
+              <td data-testid="user-profile-email">
+                {{ userStore.user.email }}
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">
+                Type
+              </th>
+              <td>
+                <span class="fr-badge fr-mr-1w" :class="AdminLevelWordingBadgeClass[userStore.adminLevel]" data-testid="user-profile-type">
+                  {{ AdminLevelWording[userStore.adminLevel] }}
+                </span>
+              </td>
+            </tr>
+          </DsfrTable>
         </div>
       </div>
     </div>
