9de60607b1c2e05ce6ab6fc76cea549249688a46
Thomas Bernard <thomashbernard@gmail.com>
2025-12-04 13:09:20 +0100

    feat: :sparkles: allow applications search sharing by using url filters

--- frontend/src/components/search/ComplianceFilter.vue
+++ frontend/src/components/search/ComplianceFilter.vue
@@ -1,43 +1,31 @@
 <script lang="ts" setup>
-import { useApplicationSearchStore } from '@/stores/applicationSearchStore';
-import type { ComplianceType } from '@/composables/use-dictionary';
+import { useApplicationSearch } from "@/composables/use-application-search";
+import type { ComplianceType } from "@/composables/use-dictionary";
 
-
-const searchStore = useApplicationSearchStore();
+const { filters, setFilter } = useApplicationSearch();
 const complianceOptions: ComplianceType[] = ["dima", "pdma", "homologation", "rgaa", "dsfr"];
 
-function toggleCompliance(value: ComplianceType, event: Event) {
-  const checked = (event.target as HTMLInputElement).checked;
-
-  const selected = new Set<ComplianceType>(searchStore.filters.compliance__in || []);
-  if (checked) {
-    selected.add(value);
-  } else {
-    selected.delete(value);
-  }
-
-  searchStore.setFilter({ compliance__in: Array.from(selected) });
+function toggleCompliance(value: ComplianceType, checked: boolean) {
+  const selected = new Set<ComplianceType>(filters.value.compliance__in || []);
+  checked ? selected.add(value) : selected.delete(value);
+  setFilter({ compliance__in: Array.from(selected) });
 }
-
 </script>
-    
+
 <template>
   <div>
-    <legend class="fr-label fr-mb-2w">
-      Conformité RGAA
-    </legend>
+    <legend class="fr-label fr-mb-2w">Conformité RGAA</legend>
     <div data-testid="compliance-filter">
       <label v-for="option in complianceOptions" :key="option" class="checkbox-item">
         <input
           type="checkbox"
           :value="option"
-          :checked="searchStore.filters.compliance__in?.includes(option)"
+          :checked="filters.compliance__in?.includes(option)"
           :data-testid="`compliance-option-${option}`"
-          @change="(e) => toggleCompliance(option, e)"
+          @change="(e) => toggleCompliance(option, (e.target as HTMLInputElement).checked)"
         >
         {{ option.toUpperCase() }}
       </label>
     </div>
-
   </div>
 </template>
