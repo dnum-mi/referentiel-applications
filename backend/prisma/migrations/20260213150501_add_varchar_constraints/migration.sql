b788f36f667d41efd07d3b69ad4897005fed8366
Thomas <thomashbernard@gmail.com>
2026-02-16 10:13:16 +0100

    Remove warning for enum values in migration.sql
    Removed warning about enum values on `Status`.

--- backend/prisma/migrations/20260213150501_add_varchar_constraints/migration.sql
+++ backend/prisma/migrations/20260213150501_add_varchar_constraints/migration.sql
@@ -1,7 +1,6 @@
 /*
   Warnings:
 
-  - The values [to_validate] on the enum `Status` will be removed. If these variants are still used in the database, this will fail.
   - You are about to alter the column `email` on the `Actor` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
   - You are about to alter the column `firstname` on the `Actor` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(100)`.
   - You are about to alter the column `lastname` on the `Actor` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(100)`.
