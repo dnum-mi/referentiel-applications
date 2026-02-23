bb4cc657ecc1b9a96066dbdc2be369c617ba4acd
code-oz <ozannethomasplay@gmail.com>
2026-02-23 13:50:38 +0100

    feat: remove business repository and use base service

--- backend/src/business-division/business-division.module.ts
+++ backend/src/business-division/business-division.module.ts
@@ -1,7 +1,6 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "src/prisma/prisma.module";
 
-import { BusinessDivisionRepository } from "./business-division.repository";
 import { BusinessDivisionService } from "./business-division.service";
 import { PrismaQueryBuilder } from "./prisma-query-builder.service";
 import { BusinessDivisionController } from "./business-division.controller";
@@ -9,11 +8,7 @@ import { BusinessDivisionController } from "./business-division.controller";
 @Module({
   imports: [PrismaModule],
   controllers: [BusinessDivisionController],
-  providers: [
-    BusinessDivisionRepository,
-    BusinessDivisionService,
-    PrismaQueryBuilder,
-  ],
+  providers: [BusinessDivisionService, PrismaQueryBuilder],
   exports: [BusinessDivisionService],
 })
 export class BusinessDivisionModule {}
--- backend/src/business-division/business-division.service.ts
+++ backend/src/business-division/business-division.service.ts
@@ -1,27 +1,33 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
-import { BusinessDivisionRepository } from "./business-division.repository";
-import { BusinessDivisionFiltersDto } from "./dto/business-division.dto";
+import { Injectable } from "@nestjs/common";
+import { BaseService } from "src/common/base.service";
+import { PrismaService } from "src/prisma/prisma.service";
+import {
+  BusinessDivisionDTO,
+  BusinessDivisionFiltersDto,
+} from "./dto/business-division.dto";
 import { PrismaQueryBuilder } from "./prisma-query-builder.service";
+import { Prisma } from "@prisma/client";
 
 @Injectable()
-export class BusinessDivisionService {
+export class BusinessDivisionService extends BaseService<
+  BusinessDivisionDTO,
+  Prisma.BusinessDivisionDelegate
+> {
   constructor(
-    private readonly repository: BusinessDivisionRepository,
     private readonly queryBuilder: PrismaQueryBuilder,
-  ) {}
+    protected readonly prisma: PrismaService,
+  ) {
+    super(prisma.businessDivision, prisma);
+  }
 
-  public async findById(id: string) {
-    const businessDivision = await this.repository.findFirstById(id);
-    if (!businessDivision) {
-      throw new NotFoundException(`Business division with ${id} not found`);
-    }
-    return businessDivision;
+  public findById(id: string) {
+    return this.findOne(id);
   }
 
   public async search(searchParams: BusinessDivisionFiltersDto) {
     const where = this.queryBuilder.buildSearchWhere(searchParams);
     const orderBy = this.queryBuilder.buildOrderBy(searchParams);
-    return this.repository.findAll({
+    return this.findAllPaginated({
       where,
       orderBy,
       page: searchParams.page,
