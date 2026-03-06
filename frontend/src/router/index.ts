7ade5cd7079873de939cba646eb7e6805c1fa4e1
tailebm <131550467+tailebm@users.noreply.github.com>
2026-03-06 12:09:06 +0100

    fix(admin): :bug: non admin redirection with admin level (#1554)
    * fix(admin): :bug: non admin redirection with admin level
    
    * test(admin): :white_check_mark: ensure user is loaded before checking admin access in router guard

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import { routeNames } from "./route-names";
 import { USER_MANAGER } from "@/services/authentication";
+import { AdminLevel } from "@/models/user";
+import { useUserStore } from "@/stores/userStore";
 
 const oidcRoutes = [
   {
@@ -128,6 +130,8 @@ const router = createRouter({
 
 // Guard to protect routes that require authentication
 router.beforeEach(async (to) => {
+  const userStore = useUserStore();
+
   if (to.meta.requiresAuth) {
     const user = await USER_MANAGER.getUser();
     if (!user) {
@@ -136,10 +140,12 @@ router.beforeEach(async (to) => {
       return { name: routeNames.SIGNIN };
     }
 
-    if (to.meta.requiresAdmin) {
-      const groups = user?.profile?.groups;
+    if (!userStore.user) {
+      await userStore.fetchUser();
+    }
 
-      if (!Array.isArray(groups) || !groups.includes("admin")) {
+    if (to.meta.requiresAdmin) {
+      if (userStore.adminLevel < AdminLevel.ADMIN) {
         return { path: "/" };
       }
     }
