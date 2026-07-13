7b6c505f9c3e5d7527837c45d2f91adac27a026d
Julien Vadic <julien.leotier@gmail.com>
2026-07-13 10:10:11 +0200

    fix(a11y): RGAA lot L — contraste couleurs & éléments graphiques (#1782)

--- frontend/src/views/MetadataDetailPage.vue
+++ frontend/src/views/MetadataDetailPage.vue
@@ -177,15 +177,15 @@ onMounted(() => {
 <style scoped>
 .add {
   background-color: #e6f8ea;
-  color: #1aa779;
+  color: #0a6c4d;
 }
 .update {
   background-color: #f8f3e6;
-  color: #a7791a;
+  color: #6d4e0f;
 }
 .delete {
   background-color: #f8e6e6;
-  color: #a71a1a;
+  color: #8e1212;
 }
 
 .metadata-description {
